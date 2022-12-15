5be66aeba142e3452dca500a73beaa981626feba
srijal <srijal@planittesting.com>
2022-12-15 10:40:36 +1000

    basic book render in browser

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
 
 function App() {
+    const [books, setBooks] = useState([]);
+
+    const createBook = (title) => {
+        const updatedBooks = [
+            ...books, { id: Math.round(Math.random() * 999), title: title }
+        ];
+        setBooks(updatedBooks)
+    };
+
     return (
-        <div>App</div>
+        <div className="app">
+            <BookList books={books} />
+            <BookCreate onCreate={createBook} />
+        </div>
     )
 }
 export default App
--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,6 +1,25 @@
-function BookCreate() {
+import { useState } from "react";
+
+function BookCreate({ onCreate }) {
+    const [title, setTitle] = useState('');
+    const handleChange = (event) => {
+        setTitle(event.target.value);
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onCreate(title);
+        setTitle('');
+    }
     return (
-        <div>BookCreate</div>
+        <div className="book-create">
+            <h3>Add a Book</h3>
+            <form onSubmit={handleSubmit}>
+                <label>Title</label>
+                <input className="input" value={title} onChange={handleChange} />
+                <button className="button" >Create Book</button>
+            </form>
+
+        </div>
     )
 }
 export default BookCreate
